aad5ac6a73299239b7bbacdef29bd4b6c23d8aa3
32tarcan <tarcann.bahadir@gmail.com>
2024-05-04 23:55:59 +0300

    Music File created.

---
+++ MusicApp/MusicView.swift
@@ -0,0 +1,18 @@
+//
+//  MusicView.swift
+//  MusicApp
+//
+//  Created by Sakans on 4.05.2024.
+//
+
+import SwiftUI
+
+struct MusicView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MusicView()
+}
