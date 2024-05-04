d5956a36c8c26785b5c696a9eb0b948da76308d2
32tarcan <tarcann.bahadir@gmail.com>
2024-05-04 23:57:09 +0300

    adding buttons and some changes.

--- MusicApp/MusicInfo.swift
+++ MusicApp/MusicInfo.swift
@@ -19,11 +19,14 @@ struct MusicInfo: View {
                 if !expandSheet {
                     GeometryReader {
                         let size = $0.size
-                        Image("music 1")
+                        Image("Music 1")
                             .resizable()
                             .aspectRatio(contentMode: .fill)
                             .frame(width: size.width, height: size.height)
-                            .clipShape(.rect(cornerRadius: 10, style: .continuous))
+                            .clipShape(.rect(cornerRadius: 60, style: .continuous))
+                        
+                        CircleProgressView(progress: 40)
+                            .frame(width: size.width, height: size.height)
                     }
                     .matchedGeometryEffect(id: "SONGCOVER", in: animation)
                 }
@@ -36,11 +39,49 @@ struct MusicInfo: View {
                 .padding(.horizontal, 15)
             
             Spacer()
+            
+            Button {
+                
+            } label: {
+                Image(systemName: "pause.fill")
+                    .font(.title2)
+                    .foregroundStyle(.black)
+                    .padding()
+                    .background(.white)
+                    .clipShape(Circle())
+            }
+        }
+            .foregroundStyle(.white)
+            .padding()
+            .frame(height: 80)
+            .onTapGesture {
+                withAnimation(.easeInOut(duration: 0.3)) {
+                    expandSheet = true
+                }
+            }
         }
     }
-}
 
-#Preview {
-    ContentView()
-        .preferredColorScheme(.dark)
-}
+    
+    #Preview {
+        ContentView()
+            .preferredColorScheme(.dark)
+    }
+    
+    struct CircleProgressView: View {
+        
+        let progress: Double
+        
+        var body: some View {
+            ZStack {
+                Circle()
+                    .stroke(Color.clear,
+                            lineWidth: 4)
+                Circle()
+                    .trim(from: 0, to: 0.25)
+                    .stroke(Color.blue, style: StrokeStyle(
+                        lineWidth: 4, lineCap: .round))
+                    .rotationEffect(.degrees(-90))
+            }
+        }
+    }
