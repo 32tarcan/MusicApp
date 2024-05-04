49f7652e85dc8184ed49076b65338a35fdc0024e
32tarcan <tarcann.bahadir@gmail.com>
2024-05-04 21:44:25 +0300

    Music File created.

---
+++ MusicApp/MusicInfo.swift
@@ -0,0 +1,18 @@
+//
+//  MusicInfo.swift
+//  MusicApp
+//
+//  Created by Sakans on 4.05.2024.
+//
+
+import SwiftUI
+
+struct MusicInfo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MusicInfo()
+}
