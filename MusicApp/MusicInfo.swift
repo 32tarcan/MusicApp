13434ad6e743b1f690b873512443b90b6492d050
32tarcan <tarcann.bahadir@gmail.com>
2024-05-04 21:51:06 +0300

    Music adding.

--- MusicApp/MusicInfo.swift
+++ MusicApp/MusicInfo.swift
@@ -8,11 +8,39 @@
 import SwiftUI
 
 struct MusicInfo: View {
+    
+    @Binding var expandSheet: Bool
+    var animation: Namespace.ID
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 0) {
+            //
+            ZStack {
+                if !expandSheet {
+                    GeometryReader {
+                        let size = $0.size
+                        Image("music 1")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: size.width, height: size.height)
+                            .clipShape(.rect(cornerRadius: 10, style: .continuous))
+                    }
+                    .matchedGeometryEffect(id: "SONGCOVER", in: animation)
+                }
+            }
+            .frame(width: 55, height: 55)
+            
+            Text("Song Title")
+                .fontWeight(.semibold)
+                .lineLimit(1)
+                .padding(.horizontal, 15)
+            
+            Spacer()
+        }
     }
 }
 
 #Preview {
-    MusicInfo()
+    ContentView()
+        .preferredColorScheme(.dark)
 }
