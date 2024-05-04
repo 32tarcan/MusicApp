8734f76d2036caf481d3f62e89de0f6dc9df3f78
32tarcan <tarcann.bahadir@gmail.com>
2024-05-04 21:44:00 +0300

    ContentView UI created.

--- MusicApp/ContentView.swift
+++ MusicApp/ContentView.swift
@@ -8,17 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var expandSheet = false
+    @Namespace private var animation
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            Home()
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+                .toolbarBackground(.visible, for: .tabBar)
+                .toolbarBackground(.ultraThickMaterial, for: .tabBar)
+            
+            Text("Search")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                    Text("Search")
+                }
+            Text("Playlists")
+                .tabItem {
+                    Image(systemName: "play.rectangle.on.rectangle")
+                    Text("Playlists")
+                }
         }
-        .padding()
+        .tint(.white)
+        .safeAreaInset(edge: .bottom) {
+            // Custom Bottom Sheet
+        }.overlay {
+            if expandSheet {
+                
+            }
+        }
     }
+    @ViewBuilder
+    func CustomBottomSheet() -> some View {
+        ZStack {
+            if expandSheet {
+                Rectangle()
+                    .fill(.clear)
+            } else {
+                Rectangle()
+                    .fill(.ultraThickMaterial)
+                    .overlay {
+                        // Music Info
+                    }.matchedGeometryEffect(id: "BACKGROUNDVIEW", in: animation)
+            }
+        }
+        .frame(height: 80)
+        .overlay(alignment: .bottom) {
+            Rectangle()
+                .fill(.gray.opacity(0.3))
+                .frame(height: 1)
+                .offset(y: -5)
+        }
+        .offset(y: -59)
+    }
+    
 }
 
 #Preview {
     ContentView()
+        .preferredColorScheme(.dark)
 }
