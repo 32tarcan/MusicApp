2fa295bd2b02f0ac15d6bff3498cc99c61b6de6a
32tarcan <tarcann.bahadir@gmail.com>
2024-05-04 21:33:07 +0300

    Home File created.

---
+++ MusicApp/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  MusicApp
+//
+//  Created by Sakans on 4.05.2024.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home()
+}
