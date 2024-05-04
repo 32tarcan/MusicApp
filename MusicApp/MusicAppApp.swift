4e69b50b06770b221e67fa0340d58707b4b1bf5d
32tarcan <tarcann.bahadir@gmail.com>
2024-05-04 21:14:49 +0300

    Initial Commit

---
+++ MusicApp/MusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAppApp.swift
+//  MusicApp
+//
+//  Created by Sakans on 4.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct MusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
